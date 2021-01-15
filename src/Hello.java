ed0a934c417332adb11bbc06fa12a5dca881547f
garlam <garlam_au@qq.com>
2021-01-16 01:50:39 +0800

    update

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,17 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello Word !");
+        Hello h = new Hello();
+        h.before();
+        h.after();
+    }
+
+
+    public void before() {
+        System.out.println("before");
+    }
+
+    public void after() {
+        System.out.println("after");
     }
 }
