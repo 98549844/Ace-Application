8ccadb3a8b7020b3d6903b2d72b4794594eb5e91
garlam <garlam_au@qq.com>
2021-04-24 23:21:19 +0800

    update

--- src/main/java/Ace.java
+++ src/main/java/Ace.java
@@ -7,6 +7,7 @@ public class Ace {
     public static void main(String[] args) throws IOException {
       String s = "-1460";
         System.out.println(s.indexOf(","));
+        System.out.println("Garlam");
 
     }
 
