2ff82562cbae827bc8a30234cf565809b8db9bbd
garlam <garlam_au@qq.com>
2021-05-10 15:24:24 +0800

    udpate

--- src/main/java/Ace.java
+++ src/main/java/Ace.java
@@ -1,15 +1,37 @@
 import entity.LogEnt;
 
 import java.io.IOException;
+import java.util.Set;
 
 public class Ace {
 
-    public static void main(String[] args) throws IOException {
-      String s = "-1460";
-        System.out.println(s.indexOf(","));
-        System.out.println("Garlam");
+	private static Object Set;
+	private static Object String;
 
-    }
+	public static void main(String[] args) throws IOException {
+		String a = "A / B / C / 111";
+		String b = "A / C / B / 111";
+		String c = "B / A / C / 111";
+		String d = "B / C / A / 111";
+		String e = "C / B / A / 111";
+		String f = "C / A / B / 111";
+
+		String[] as = a.split("/");
+		String[] bs = a.split("/");
+		String[] cs = a.split("/");
+		String[] ds = a.split("/");
+		String[] es = a.split("/");
+		String[] fs = a.split("/");
+
+
+
+
+
+
+		System.out.println("231321321");
+		System.out.print("231321321");
+
+	}
 
 
 }
